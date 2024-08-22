39ac617ef33ac73033b3d96db3ab67e9b928a8e3
Sandesha pokhrel <pokhrel.sandesha1@gmail.com>
2024-08-22 10:06:13 +0545

    Add inheritanceDemo

---
+++ DotNetIntermediate/interfaceDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Person
+{
+    protected string name;
+    protected string gender;
+    public Person()
+    {
+        name = "sandesha";
+        gender = "female";
+
+    }
+    public Person(string name, string gender) : base()
+    {
+        this.name = name;
+        this.gender = gender;
+    }
+    public void show()
+    {
+        Console.WriteLine("Name=" + name);
+        Console.WriteLine("gender=" + gender);
+    }
+}
+    class Student: Person
+    {
+        public string studentID;
+    //constructor for student class
+        public Student (string studentID, string name, string gender) : base(name, gender)
+        {
+            this.studentID = studentID;
+        }
+
+        public void display()
+        {
+            show(); 
+            Console.WriteLine("Student ID = " + studentID);
+        }
+    }
+
+
---
+++ DotNetIntermediate/interfaceDemo/student.cs
@@ -0,0 +1,14 @@
+﻿namespace one
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("1", "golu", "male");
+            s.display();
+
+            Console.ReadLine(); 
+        }
+    }
+
+}
