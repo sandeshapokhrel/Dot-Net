59d247b4a342db6997eb64662687cde22c56ae80
Sandesha pokhrel <pokhrel.sandesha1@gmail.com>
2024-09-22 08:19:19 +0545

    add lab3

---
+++ LABs/Lab3/3.2_MultiLevelInheritance/3.2_MultiLevelInheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿//Write a program to show the multilevel inheritance. 
+using System.Runtime.InteropServices;
+
+namespace _3._2_MultiLevelInheritance
+{
+    internal class Program
+    {
+        public class Animal
+        {
+            public void MakesSound()
+            {
+                Console.WriteLine("Animal MakesSound");
+            }
+        }
+        public class Dog:Animal
+        {
+            public void Barks()
+            {
+                Console.WriteLine("Dog Barks");
+            }
+        }
+        public class Cat:Dog
+        {
+            public void Meow()
+            {
+                Console.WriteLine("Cat Meows");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cat animal = new Cat();
+            animal.MakesSound();
+            animal.Meow();
+            animal.Barks();
+           
+        }
+    }
+}
