002a39b1617a761c46d2a1c0b4ec50b48397c336
Sandesha pokhrel <pokhrel.sandesha1@gmail.com>
2024-09-22 22:01:47 +0545

    ADD lab3.4 MultiLevelInheritance

---
+++ LABs/Lab3_Inheritance/3.3_HierarchicalInheritance/3.3_HierarchicalInheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿// Write a program to show the hierarchical inheritance. 
+using System.ComponentModel.DataAnnotations;
+
+namespace _3._3_HierarchicalInheritance_
+{
+    internal class Program
+    {  public class Employee
+        {
+            public string Name { get; set; }
+            public void DisplayInfo ()
+            {
+                Console.Write($"Name:{Name} ");
+            }
+        }
+        public class PartTimeEmployee:Employee
+        {
+            public void DisplayPartTimeInfo()
+            {
+                DisplayInfo();
+                Console.WriteLine("Part Time Employee");
+            }
+        }
+        public class FullTime : Employee 
+        { 
+            public void DisplayFullTimeInfo() 
+            {
+              DisplayInfo();
+              Console.WriteLine("Full Time Employee");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            PartTimeEmployee emp1 = new PartTimeEmployee { Name = "sudip"};
+            FullTime emp2 = new FullTime { Name= "Megan" } ;
+            emp1.DisplayPartTimeInfo();
+            emp2.DisplayFullTimeInfo();
+
+        }
+    }
+}
---
+++ LABs/Lab3_Inheritance/3.4_MultipleInheritance/3.4_MultipleInheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿//Write a program to implement the features of multiple inheritance. 
+namespace _3._4_MultipleInheritance
+{
+    internal class Program
+    {   
+        public class vechilee
+        {
+            public void Stop()
+            {
+                Console.WriteLine("Stopped...");
+            }
+        }
+        public interface  Ivechile
+        {
+            public void Drive ();
+        }
+        public class RangeRover: vechilee, Ivechile
+        {
+            public void Drive()
+            {
+                Console.WriteLine("Driving....");
+            }
+            
+
+
+        }
+       
+        static void Main(string[] args)
+        {
+            Ivechile obj1 = new RangeRover();
+            obj1.Drive();
+            vechilee obj2 = new vechilee();
+            obj2.Stop();
+        }
+    }
+}
