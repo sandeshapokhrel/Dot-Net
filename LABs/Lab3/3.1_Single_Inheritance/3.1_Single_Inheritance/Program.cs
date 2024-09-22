f572de71c98a7c9d2c0cc6b6c509ab6edca68ac0
Sandesha pokhrel <pokhrel.sandesha1@gmail.com>
2024-09-22 08:18:06 +0545

    add lab 3.2 multilevel inheritance

---
+++ LABs/Lab3/3.1_Single_Inheritance/3.1_Single_Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _3._1_Single_Inheritance
+{
+    internal class Program
+    {
+        public class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Animal eats");
+            }
+            public void Stop()
+            {
+                Console.WriteLine("Animal Stop Eating");
+            }
+        }
+        public class dog : Animal
+        {
+            public void Eat()
+            
+                {
+                base.Eat();
+
+               // Console.WriteLine("dog is eating");
+                }
+            
+            public new void Stop()
+            {
+                base.Stop();
+
+                //Console.WriteLine("Dog stopped eating");
+            }
+        }
+        class programMain
+        {
+            static void Main(string[] args)
+            {
+                Animal animal2 = new dog();
+                animal2.Eat();
+                animal2.Stop();
+            }
+        }
+    }
+}
