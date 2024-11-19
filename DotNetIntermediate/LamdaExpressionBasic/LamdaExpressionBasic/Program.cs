af6225474d080e629af655f1979fdf5400a1f068
Sandesha pokhrel <pokhrel.sandesha1@gmail.com>
2024-11-19 07:02:42 +0545

    LambdaExpressionBasic Example

---
+++ DotNetIntermediate/LamdaExpressionBasic/LamdaExpressionBasic/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LamdaExpressionBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // Expression Lambda
+
+            Func<int, int> abc= x => x * x;
+            int a = abc(5);
+            Console.WriteLine(a);
+
+            //Statement Lambda
+
+            Func<int, int> abcd = x => { return x * x; };
+            int b = abcd(5);
+            Console.WriteLine(b);
+        }
+    }
+}
