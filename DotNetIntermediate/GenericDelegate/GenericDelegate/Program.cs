80f2432444dc8aab010aab54eff984a41df9aacf
Sandesha pokhrel <pokhrel.sandesha1@gmail.com>
2024-11-20 06:57:35 +0545

    GenericDelegateProgrem

---
+++ DotNetIntermediate/GenericDelegate/GenericDelegate/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    // Func Generic Delegate
+            Func<int, string> check = a => (a % 2 == 0) ? "even" : "odd";
+            Console.WriteLine(check(2)); //returns a value 
+
+            // Action Delegate
+            Action<int> checkAndPrint = a => Console.WriteLine((a % 2 == 0) ? "even" : "odd");
+            checkAndPrint(2); //doesnt return a value 
+
+            // Predicate Delegeate
+            Predicate<int> isEven = a => a % 2 == 0;
+            Console.WriteLine(isEven(2)); // gives boolen output.
+
+
+        }
+    }
+}
--- DotNetIntermediate/LamdaExpressionBasic/LamdaExpressionBasic/Program.cs
+++ DotNetIntermediate/LamdaExpressionBasic/LamdaExpressionBasic/Program.cs
@@ -3,17 +3,35 @@
     internal class Program
     {
         static void Main(string[] args)
-        {   // Expression Lambda
-
-            Func<int, int> abc= x => x * x;
-            int a = abc(5);
+        {
+            // Expression Lambda
+            Func<int, int> abc = x => x * x; // Lambda function to square a number
+            int a = abc(5); // Call the function with input 5
             Console.WriteLine(a);
 
-            //Statement Lambda
-
-            Func<int, int> abcd = x => { return x * x; };
-            int b = abcd(5);
+            // Statement Lambda
+            Func<int, int> abcd = x => { return x * x; }; // Lambda function to square a number
+            int b = abcd(5); // Call the function with input 5
             Console.WriteLine(b);
+
+            // Odd or Even
+            Func<int, int> number = x => { return x * x; }; // Lambda function to square a number
+            int check = number(5); // Call the function with input 5
+            if (check % 2 == 0) 
+            {
+                Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("Odd number");
+            }
+            Console.WriteLine(check); 
+
+          
+            //Func<int, string> checkAnother = D => (D % 2 == 0) ? "Even" : "Odd";
+            //Another Method
+            var v = (int a) => (a % 2 == 0) ? "even" : "odd";
+            Console.WriteLine(v(2));
         }
     }
 }
